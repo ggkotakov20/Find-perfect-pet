d2fd629f1bc025729e5de22049fdd0e1baa8e1f6
ggkotakov20 <ggkotakov20@codingburgas.bg>
2023-06-02 23:48:30 +0300

    Add app icon

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -27,7 +27,6 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    print('$currentIndex');
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
